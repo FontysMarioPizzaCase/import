6c1c15ec6d4568808308333d083a506f15af8546
Dennis van der Veeke <mrdienns@dyescape.com>
2021-02-25 10:24:13 +0100

    setup basic (blank) spring boot starter for dominos

---
+++ dominos/src/main/java/me/fontys/semester4/dominos/DominosImport.java
@@ -0,0 +1,18 @@
+package me.fontys.semester4.dominos;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominosImport implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DominosImport.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
