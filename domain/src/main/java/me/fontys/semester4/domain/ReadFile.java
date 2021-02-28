5a52ceb9d9ae054ad28f75816181f34394a627fe
Mycha <m.devrees@gmail.com>
2021-02-28 20:06:05 +0100

    split readfile from dominos into domain, import stores txt in seperate function

--- domain/src/main/java/me/fontys/semester4/domain/ReadFile.java
+++ domain/src/main/java/me/fontys/semester4/domain/ReadFile.java
@@ -1,4 +1,4 @@
-package me.fontys.semester4.dominos;
+package me.fontys.semester4.domain;
 
 import java.io.File;
 import java.io.FileNotFoundException;
---
+++ dominos/src/main/java/me/fontys/semester4/dominos/Store.java
@@ -0,0 +1,6 @@
+package me.fontys.semester4.dominos;
+
+public class Store {
+    public int id;
+    public String name;
+}
