5de0dd5d702f84948ac40705a97e07120623ef14
Mycha <m.devrees@gmail.com>
2021-03-03 20:20:37 +0100

    make store import work

--- dominos/src/main/java/me/fontys/semester4/dominos/configuration/data/store/StoreImporter.java
+++ dominos/src/main/java/me/fontys/semester4/dominos/configuration/data/store/StoreImporter.java
@@ -1,6 +1,7 @@
 package me.fontys.semester4.dominos.configuration.data.store;
 
 import me.fontys.semester4.data.entity.Store;
+import me.fontys.semester4.data.repository.StoreRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,19 +22,23 @@ public class StoreImporter {
 
     @Qualifier("stores")
     private final Resource[] stores;
-
+    private final StoreRepository storeRepository;
     private final Map<String, Integer> warnings = new HashMap<>();
 
     @Autowired
-    public StoreImporter(Resource[] stores) {
+    public StoreImporter(
+            Resource[] stores,
+            StoreRepository storeRepository
+    ) {
         this.stores = stores;
+        this.storeRepository = storeRepository;
     }
 
     public void doImport() throws IOException {
         LOGGER.info("Starting import of stores...");
 
         this.warnings.clear();
-        List<String> stores = new ArrayList<>();
+        List<Store> stores = new ArrayList<>();
 
         for (Resource resource : this.stores) {
             LOGGER.info("Reading resource " + resource.getFilename());
@@ -41,7 +46,7 @@ public class StoreImporter {
         }
 
         LOGGER.info(String.format("Inserting %s stores...", stores.size()));
-//        this.storeRepository.saveAll(orders);
+        this.storeRepository.saveAll(stores);
     }
 
     public void report() {
@@ -49,7 +54,7 @@ public class StoreImporter {
                 .reduce(0, Integer::sum);
 
         if (totalWarnings > 0) {
-            LOGGER.warn(String.format("Order import result : %s warnings", totalWarnings));
+            LOGGER.warn(String.format("Store import result : %s warnings", totalWarnings));
 
             for (Map.Entry<String, Integer> warning : this.warnings.entrySet()) {
                 LOGGER.warn(String.format("  -> %s : %s", warning.getKey(), warning.getValue()));
@@ -57,25 +62,25 @@ public class StoreImporter {
         }
     }
 
-    private List<String> processStoreResource(Resource resource) throws IOException {
+    private List<Store> processStoreResource(Resource resource) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(resource.getInputStream()));
 
         List<String> allStrings = new ArrayList<String>();
-        String str ="";
+        String str = "";
         while(true) {
-            String tmp = in.readLine();
-            if (tmp == null) {
+            String currentLine = in.readLine();
+            if (currentLine == null) {
                 break;
-            } else if (tmp.isEmpty()) {
+            } else if (currentLine.isEmpty()) {
                 if (!str.isEmpty()) {
                     allStrings.add(str);
                 }
                 str = "";
             } else {
                 if (str.isEmpty()) {
-                    str = tmp;
+                    str = currentLine;
                 } else {
-                    str += "\n" + tmp;
+                    str += "\n" + currentLine;
                 }
             }
         }
@@ -83,13 +88,14 @@ public class StoreImporter {
             allStrings.add(str);
         }
         System.out.println(allStrings.size());
+        List<Store> stores = new ArrayList<>();
         allStrings.forEach((_store) -> {
-            String lines[] = _store.split("\\r?\\n");
-            System.out.println(lines);
-            System.out.println(Arrays.toString(lines));
-            System.exit(0);
+            String[] lines = _store.split("\\r?\\n");
+            String storeName = lines[0];
+            String storeStreet = String.format("%s %s", lines[1], lines[2]);
+            stores.add(new Store(null, storeName, storeStreet));
         });
-        return allStrings;
+        return stores;
     }
 
     private void processWarning(String message) {
