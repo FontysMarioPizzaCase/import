adbd998ec1994e6f5c66b13c26ddccdf03df21e9
Mycha <m.devrees@gmail.com>
2021-03-03 18:52:58 +0100

    import stores

---
+++ dominos/src/main/java/me/fontys/semester4/dominos/configuration/data/store/StoreImportBootstrap.java
@@ -0,0 +1,28 @@
+package me.fontys.semester4.dominos.configuration.data.store;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
+@Configuration
+public class StoreImportBootstrap {
+
+    @Value("${import.order.wildcard}")
+    private final String path = "./import-data/*.txt";
+    private final ApplicationContext applicationContext;
+
+    @Autowired
+    public StoreImportBootstrap(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean(name = "stores")
+    public Resource[] getResources() throws IOException {
+        return this.applicationContext.getResources(this.path);
+    }
+}
