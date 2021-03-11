e85d50ca344e364b2a492f52b408ca327aafd828
Dennis van der Veeke <mrdienns@dyescape.com>
2021-03-11 15:25:58 +0100

    added a findByName method to StoreRepository

--- data/src/main/java/me/fontys/semester4/data/repository/StoreRepository.java
+++ data/src/main/java/me/fontys/semester4/data/repository/StoreRepository.java
@@ -4,6 +4,9 @@ import me.fontys.semester4.data.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface StoreRepository extends JpaRepository<Store, Long>, JpaSpecificationExecutor<Store> {
 
+    Optional<Store> findByName(String name);
 }
