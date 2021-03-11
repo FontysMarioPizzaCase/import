4510a89223cbf5752a028706613a273a2cb97493
Dennis van der Veeke <mrdienns@dyescape.com>
2021-03-11 15:09:51 +0100

    run store import first because order data needs stores

--- dominos/src/main/java/me/fontys/semester4/dominos/DominosImport.java
+++ dominos/src/main/java/me/fontys/semester4/dominos/DominosImport.java
@@ -43,14 +43,14 @@ public class DominosImport implements CommandLineRunner {
     public void run(String... args) throws IOException {
         long start = System.currentTimeMillis();
 
+        // Do the store import
+        this.storeImporter.doImport();
+        this.storeImporter.report();
+
         // Do the order import
         this.orderImporter.doImport();
         this.orderImporter.report();
         this.orderImporter.test();
-
-        // Do the store import
-        this.storeImporter.doImport();
-        this.storeImporter.report();
 
         // Do the postalcode import
         pcImporter.doImport();
