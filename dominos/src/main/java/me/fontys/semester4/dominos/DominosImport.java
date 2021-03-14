8791e0d8ae15eda34d7a001fded59713bdfddbc7
Dennis van der Veeke <mrdienns@dyescape.com>
2021-03-14 17:13:47 +0100

    removed unused exceptions & throw statements

--- dominos/src/main/java/me/fontys/semester4/dominos/DominosImport.java
+++ dominos/src/main/java/me/fontys/semester4/dominos/DominosImport.java
@@ -14,8 +14,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.io.IOException;
-
 @SpringBootApplication
 @EntityScan(basePackages = "me.fontys.semester4")
 @ComponentScan(basePackages = "me.fontys.semester4")
