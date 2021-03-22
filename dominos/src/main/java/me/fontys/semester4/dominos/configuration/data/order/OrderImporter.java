fb8fdf06c40494622015e709dcb148aad2e7056f
Dennis van der Veeke <mrdienns@dyescape.com>
2021-03-22 19:27:49 +0100

    cleaned up order import

--- dominos/src/main/java/me/fontys/semester4/dominos/configuration/data/order/OrderImporter.java
+++ dominos/src/main/java/me/fontys/semester4/dominos/configuration/data/order/OrderImporter.java
@@ -1,12 +1,9 @@
 package me.fontys.semester4.dominos.configuration.data.order;
 
-import me.fontys.semester4.data.entity.Order;
-import me.fontys.semester4.data.entity.Customer;
 import me.fontys.semester4.data.repository.OrderCustomOptionRepository;
 import me.fontys.semester4.data.repository.OrderProductIngredientRepository;
 import me.fontys.semester4.data.repository.OrderProductRepository;
 import me.fontys.semester4.data.repository.OrderRepository;
-import me.fontys.semester4.data.repository.CustomerRepository;
 import me.fontys.semester4.dominos.configuration.data.ImportTest;
 import me.fontys.semester4.dominos.configuration.data.order.formatter.OrderDateFormatter;
 import me.fontys.semester4.dominos.configuration.data.order.formatter.OrderPhoneNumberFormatter;
@@ -37,8 +34,6 @@ public class OrderImporter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderImporter.class);
 
-    private static final int[] ORDER_DELIMITER_COLUMN_IDS = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8};
-
     @Qualifier("orders")
     private final Resource[] orders;
     private final OrderRepository orderRepository;
@@ -46,7 +41,6 @@ public class OrderImporter {
     private final OrderCustomOptionRepository orderCustomOptionRepository;
     private final OrderProductIngredientRepository orderProductIngredientRepository;
     private final OrderTempRepository orderTempRepository;
-    private final CustomerRepository customerRepository;
     private final OrderDateFormatter orderDateFormatter;
     private final OrderPhoneNumberFormatter orderPhoneNumberFormatter;
     private final StoredProcedureExecutor storedProcedureExecutor;
@@ -59,8 +53,7 @@ public class OrderImporter {
                          OrderCustomOptionRepository orderCustomOptionRepository,
                          OrderProductIngredientRepository orderProductIngredientRepository,
                          OrderTempRepository orderTempRepository, OrderDateFormatter orderDateFormatter,
-                         CustomerRepository customerRepository, OrderPhoneNumberFormatter orderPhoneNumberFormatter,
-                         StoredProcedureExecutor storedProcedureExecutor,
+                         OrderPhoneNumberFormatter orderPhoneNumberFormatter, StoredProcedureExecutor storedProcedureExecutor,
                          @Value("classpath:procedures/create_customers.sql") Resource createCustomerStoredProcedure) {
         this.orders = orders;
         this.orderRepository = orderRepository;
@@ -69,7 +62,6 @@ public class OrderImporter {
         this.orderProductIngredientRepository = orderProductIngredientRepository;
         this.orderTempRepository = orderTempRepository;
         this.orderDateFormatter = orderDateFormatter;
-        this.customerRepository = customerRepository;
         this.orderPhoneNumberFormatter = orderPhoneNumberFormatter;
         this.storedProcedureExecutor = storedProcedureExecutor;
         this.createCustomerStoredProcedure = createCustomerStoredProcedure;
@@ -82,19 +74,15 @@ public class OrderImporter {
 
         this.warnings.clear();
         List<OrderTemp> orders = new ArrayList<>();
-        List<Customer> customers = new ArrayList<>();
 
         for (Resource resource : this.orders) {
             LOGGER.info("Reading resource " + resource.getFilename());
             orders.addAll(this.processOrderResource(resource));
-            customers.addAll(this.processCustomerResource(resource));
         }
 
         LOGGER.info(String.format("Inserting %s orders...", orders.size()));
         this.orderTempRepository.saveAll(orders);
 
-        LOGGER.info(String.format("Inserting %s customers...", customers.size()));
-        this.customerRepository.saveAll(customers);
         this.storedProcedureExecutor.executeSql("CALL create_customers()", false);
     }
 
@@ -149,29 +137,6 @@ public class OrderImporter {
             }
         }
 
-        return result;
-    }
-
-    private List<Customer> processCustomerResource(Resource resource) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
-        List<Customer> result = new ArrayList<>();
-        int rowCount = 0;
-        while (reader.ready()) {
-            rowCount++;
-            String line = reader.readLine();
-            // Ignore the initial header rows and empty lines
-            if (isHeader(rowCount)) continue;
-            if (line.isEmpty()) continue;
-
-            String[] lineDetails = line.split(";");
-
-            if (!isDelimitedOrder(lineDetails)) {
-                Customer customer = getCustomerFromLine(lineDetails);
-                if (customer == null) continue;
-
-                result.add(customer);
-            }
-        }
         return result;
     }
 
@@ -241,65 +206,5 @@ public class OrderImporter {
         quantity = quantity.trim();
 
         return Integer.parseInt(quantity);
-    }
-
-    private Customer getCustomerFromLine(String[] line) {
-
-        // If it doesn't have at least 3 fields, we can't build a customer
-        if (line.length < 8) return null;
-
-        String customerName = line[1];
-
-        String[] customerNameSplit = customerName.split(" ");
-        String firstName = customerNameSplit[0];
-        StringBuilder lastName = new StringBuilder();
-
-        for (int i = 0; i < customerNameSplit.length; i++) {
-            if (i == 0) continue;
-            lastName.append(" ").append(customerNameSplit[i]);
-        }
-
-        String email = line[3].toLowerCase();
-        String deliveryType = line[7];
-
-        if ("bezorgen".equalsIgnoreCase(deliveryType) && customerName.isEmpty()) {
-            processWarning("Order does not have any customer name");
-        }
-        if ("bezorgen".equalsIgnoreCase(deliveryType) && email.isEmpty()) {
-            processWarning("Order does not have any email");
-        }
-
-        if ("afhalen".equalsIgnoreCase(deliveryType) && (customerName.isEmpty() || email == null || email.isEmpty())) {
-
-            // It was delivery, and we have no customer name & email. We can skip this
-            return null;
-        }
-
-        if (this.customerRepository.findByEmail(email).isEmpty()) {
-            // we already have customer based on email
-            return null;
-        }
-
-        return new Customer(null, email, firstName, lastName.toString());
-    }
-
-    private void processWarning(String message) {
-        if (this.warnings.containsKey(message)) {
-            this.warnings.put(message, this.warnings.get(message) + 1);
-        } else {
-            this.warnings.put(message, 1);
-        }
-    }
-
-    private void appendDelimitedLineToOrder(String[] line, Order order) {
-
-    }
-
-    private boolean isDelimitedOrder(String[] line) {
-        for (int id : ORDER_DELIMITER_COLUMN_IDS) {
-            if (id + 1 >= line.length) continue;
-            if (!line[id].isEmpty()) return false;
-        }
-        return true;
     }
 }
