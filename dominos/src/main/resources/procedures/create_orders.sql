3a98a72e94d7eb069960ed49cdcdaa4b4d9a2f5f
Dennis van der Veeke <mrdienns@dyescape.com>
2021-03-24 20:57:43 +0100

    added further order import logic

--- dominos/src/main/resources/procedures/create_orders.sql
+++ dominos/src/main/resources/procedures/create_orders.sql
@@ -1,20 +1,25 @@
 CREATE OR REPLACE procedure create_orders()
     LANGUAGE plpgsql
-AS
-$$
+AS $$
 DECLARE
 
-    order_cursor CURSOR FOR SELECT *
-                            FROM order_import
-                                     LEFT OUTER JOIN customer c ON c.email = LOWER(order_import.email)
-                                     LEFT OUTER JOIN product p ON LOWER(p.name) = LOWER(order_import.product)
-                                     LEFT OUTER JOIN "order" o ON o.customer = c.customerid
-                                     LEFT OUTER JOIN store s on order_import.store_name = s.name;
-    order_row          RECORD;
+    order_cursor CURSOR FOR SELECT * FROM order_import
+        LEFT OUTER JOIN customer c ON c.email = LOWER(order_import.email)
+        LEFT OUTER JOIN product p ON LOWER(p.name) = LOWER(order_import.product)
+        LEFT OUTER JOIN "order" o ON o.customer = c.customerid
+        LEFT OUTER JOIN store s on order_import.store_name = s.name;
+
+    order_row RECORD;
     previous_new_order RECORD;
-    order_count        INT = 0;
-    orderline_count    INT = 0;
-    created_order_id   INT = 0;
+    ingredient_row RECORD;
+
+    ingredient_item VARCHAR;
+
+    order_count INT = 0;
+    orderline_count INT = 0;
+
+    created_order_id INT = 0;
+    created_order_product_id INT = 0;
 
 BEGIN
 
@@ -40,18 +45,43 @@ BEGIN
                     order_row.total_price, order_row.storeid)
             RETURNING "order".orderid INTO created_order_id;
 
-            INSERT INTO public.order_product (name, orderid, price, productid, quantity, taxrate)
+            INSERT INTO order_product (name, orderid, price, productid, quantity, taxrate)
             VALUES (order_row.product, created_order_id, order_row.product_price, order_row.productid,
-                    order_row.quantity, order_row.taxrate);
+                    order_row.quantity, order_row.taxrate)
+            RETURNING order_product.order_productid INTO created_order_product_id;
 
-            order_count = order_count + 1;
-            orderline_count = orderline_count + 1;
+            FOREACH ingredient_item IN ARRAY string_to_array(LOWER(order_row.extra_ingredients), ',')
+                LOOP
+
+                    SELECT INTO ingredient_row * FROM ingredient WHERE LOWER(ingredient.name) = TRIM(ingredient_item);
+
+                    IF ingredient_row.ingredientid IS NULL THEN
+                        RAISE NOTICE 'Ingredient % does not exist', ingredient_item;
+                        CONTINUE WHEN true;
+                    END IF;
+
+                    INSERT INTO order_product_ingredient (ingredientid, addprice, name, number, order_productid)
+                    VALUES (ingredient_row.ingredientid, 0, ingredient_row.name, 1, created_order_product_id);
+                END LOOP;
         ELSE
-            INSERT INTO public.order_product (name, orderid, price, productid, quantity, taxrate)
+            INSERT INTO order_product (name, orderid, price, productid, quantity, taxrate)
             VALUES (order_row.product, created_order_id, order_row.product_price, order_row.productid,
-                    order_row.quantity, order_row.taxrate);
+                    order_row.quantity, order_row.taxrate)
+            RETURNING order_product.order_productid INTO created_order_product_id;
 
-            orderline_count = orderline_count + 1;
+            FOREACH ingredient_item IN ARRAY string_to_array(LOWER(order_row.extra_ingredients), ',')
+                LOOP
+
+                    SELECT INTO ingredient_row * FROM ingredient WHERE LOWER(ingredient.name) = TRIM(ingredient_item);
+
+                    IF ingredient_row.ingredientid IS NULL THEN
+                        RAISE NOTICE 'Ingredient % does not exist', ingredient_item;
+                        CONTINUE WHEN true;
+                    END IF;
+
+                    INSERT INTO order_product_ingredient (ingredientid, addprice, name, number, order_productid)
+                    VALUES (ingredient_row.ingredientid, 0, ingredient_row.name, 1, created_order_product_id);
+                END LOOP;
         END IF;
 
     END LOOP;
@@ -61,5 +91,4 @@ BEGIN
     RAISE NOTICE 'New orders: %', order_count;
     RAISE NOTICE 'New order lines: %', orderline_count;
 
-END
-$$;
+END $$;
