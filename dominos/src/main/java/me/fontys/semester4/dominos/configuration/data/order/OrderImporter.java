fe1cdcbb34781366222baae809ea3d81c25418da
Mycha <m.devrees@gmail.com>
2021-03-24 22:06:55 +0100

    log when stored procedures are called and fix storeimporter trim

--- dominos/src/main/java/me/fontys/semester4/dominos/configuration/data/order/OrderImporter.java
+++ dominos/src/main/java/me/fontys/semester4/dominos/configuration/data/order/OrderImporter.java
@@ -94,8 +94,11 @@ public class OrderImporter {
         LOGGER.info(String.format("Inserting %s orders...", orders.size()));
         this.orderTempRepository.saveAll(orders);
 
+        LOGGER.info("Calling create_customers()");
         this.storedProcedureExecutor.executeSql("CALL create_customers()", false);
+        LOGGER.info("Calling create_addresses()");
         this.storedProcedureExecutor.executeSql("CALL create_addresses()", false);
+        LOGGER.info("Calling create_orders()");
         this.storedProcedureExecutor.executeSql("CALL create_orders()", false);
         LOGGER.info("Process order coupons...");
         processCouponsProc.Execute();
