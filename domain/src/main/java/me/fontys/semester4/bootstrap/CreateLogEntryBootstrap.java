1586f1acb891c86b2464629fd9bfba0b8d60a7bf
Mycha <m.devrees@gmail.com>
2021-03-29 19:52:34 +0200

    add createLogEntryBean to use domain stored procedure

---
+++ domain/src/main/java/me/fontys/semester4/bootstrap/CreateLogEntryBootstrap.java
@@ -0,0 +1,23 @@
+package me.fontys.semester4.bootstrap;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+@Configuration
+public class CreateLogEntryBootstrap {
+
+    private final Resource createLogEntry;
+    public CreateLogEntryBootstrap(@Value("classpath:createLogEntry.sql") Resource createLogEntry) {
+        this.createLogEntry = createLogEntry;
+    }
+
+    @Bean(name = "createLogEntryBean")
+    public Resource getResources() throws IOException {
+        return this.createLogEntry;
+    }
+}
